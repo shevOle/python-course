7074e46ae09f29a33bfa18d979155c4d160d160b
Oleg Shevchenko <shevchenkoo.js@gmail.com>
2024-07-28 18:09:19 +0300

    feat: add more printing

--- 00-intro.py
+++ 00-intro.py
@@ -1,1 +1,13 @@
-print('hello world')
+print('string:', 'hello world')
+
+print('strings concatenation:', 'hello' + ' world')
+
+print('sum:', 1 + 1)
+
+print('compare numbers:', 1 == 1)
+
+print('compare symbols:', 'a' == 'A')
+
+print('compare number to symbol:', 1 == '1')
+
+print('compare number to bool:', 1 == True)
