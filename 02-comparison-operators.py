f9c824f80d4508b30698756b9b95d7e16a262fd7
Oleg Shevchenko <shevchenkoo.js@gmail.com>
2024-08-03 14:14:18 +0300

    feat: add comparison operators practice

---
+++ 02-comparison-operators.py
@@ -0,0 +1,54 @@
+print('2.0 == 2', '->', 2.0 == 2)
+print()
+  
+print('3 != 3', '->', 3 != 3)
+print()
+  
+print('4 != 5', '->', 4 != 5)
+print()
+  
+print('4 > 5', '->', 4 > 5)
+print()
+  
+print('3 < 4', '->', 3 < 4)
+print()
+  
+print('3 >= 3', '->', 3 >= 3)
+print()
+  
+print('4 <= 6', '->', 4 <= 6)
+print()
+  
+print('ffAfff == ffafff', '->', 'ffAfff' == 'ffafff')
+print()
+
+print('ffAfff == ffAfff', '->', 'ffAfff' == 'ffAfff')
+print()
+  
+print('{ "a": 1 } == { "a": 1 }', '->', { "a": 1 } == { "a": 1 })
+print()
+  
+print('{ "a": { "b": 2 } } == { "a": { "b": 2 } }', '->', { "a": { "b": 2 } } == { "a": { "b": 2 } })
+print()
+  
+print('[1,2,3,4] == [1,2,3,4]', '->', [1,2,3,4] == [1,2,3,4])
+print()
+  
+print('(1,2,3) == (1,2,3)', '->', (1,2,3) == (1,2,3))
+print()
+  
+print('set([2,2,3,4,2,6]) == set([2,2,3,4,2,6])', '->', set([2,2,3,4,2,6]) == set([2,2,3,4,2,6]))
+print()
+  
+# errored out
+# print('{ } >= { "a":1 }', '->', { } >= { "a":1 })
+# print()
+  
+# errored out
+# print('"4" > 2', '->', "4" > 2)
+# print()
+  
+print('"4" == 4', '->', "4" == 4)
+print()
+
+  
