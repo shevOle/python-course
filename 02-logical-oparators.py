49b346404372cc5040ce1346d7539c6bc3439a8f
Oleg Shevchenko <shevchenkoo.js@gmail.com>
2024-08-03 14:22:54 +0300

    feat: add logical operators practice

---
+++ 02-logical-oparators.py
@@ -0,0 +1,44 @@
+print('1 < 2 < 3', '->', 1 < 2 < 3)
+print()
+ 
+print('1 < 2 > 3', '->', 1 < 2 > 3)
+print()
+ 
+print('1 < 2 and 2 < 3', '->', 1 < 2 and 2 < 3)
+print()
+ 
+print('1 < 2 and 2 > 3', '->', 1 < 2 and 2 > 3)
+print()
+ 
+print('2 == 2 and 3 > 1', '->', 2 == 2 and 3 > 1)
+print()
+ 
+print('2 > 4 or 5 < 10', '->', 2 > 4 or 5 < 10)
+print()
+ 
+print('2 > 4 or 5 > 10', '->', 2 > 4 or 5 < 10)
+print()
+ 
+print('not(2 > 3)', '->', not(2 > 3))
+print()
+ 
+print('not 2 > 3', '->', not 2 > 3)
+print()
+ 
+print('not({ })', '->', not({ }))
+print()
+ 
+print('not({ "a": 1 })', '->', not({ "a": 1 }))
+print()
+
+ 
+print('not([])', '->', not([]))
+print()
+ 
+print('not([1,2])', '->', not([1,2]))
+print()
+ 
+print('not not({ "a": 1 })', '->', not not({ "a": 1 }))
+print()
+ 
+ 
