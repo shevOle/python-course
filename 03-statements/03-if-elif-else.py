1b64bfa3049a554d2b52beb88dd4e2d4ad5d04b1
Oleg Shevchenko <shevchenkoo.js@gmail.com>
2024-08-04 18:30:43 +0300

    feat: add practice code for if-elif-else lesson

---
+++ 03-statements/03-if-elif-else.py
@@ -0,0 +1,20 @@
+if True:
+    print('first if, printing')
+
+if False:
+    print('won\'t be there')
+else:
+    print('from else block')
+
+if False:
+    print('nope')
+elif True:
+    print('from elif block')
+
+if False:
+    print('again, no')
+elif False:
+    print('not gonna do that')
+else:
+    print('here I come')
+
