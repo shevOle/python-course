60b93efd4d32f5a2a0f93880676840792f632963
Oleg Shevchenko <shevchenkoo.js@gmail.com>
2024-08-09 20:06:03 +0300

    feat: add more practice code to functions chapter

--- 04-functions/04-def-keyword.py
+++ 04-functions/04-def-keyword.py
@@ -1,3 +1,10 @@
+def print_hint(name, value):
+    """
+    Prints value with name formatted
+    """
+    print(name, ' -> ', value)
+    print()
+
 def print_hello(name):
     """
     Prints greeting to the person
@@ -5,6 +12,7 @@ def print_hello(name):
     print("Hello, " + name + "!")
 
 print_hello('Oleh')
+print()
 
 def add_squares(num1, num2):
     """
@@ -12,6 +20,7 @@ def add_squares(num1, num2):
     """
     print(num1**2 + num2**2)
 add_squares(5, 22)
+print()
 
 def normalize_name(name):
     """
@@ -19,3 +28,29 @@ def normalize_name(name):
     """
     print(str(name).lower())
 normalize_name('Some Name With Uppercase')
+print()
+
+# will fail with error, required argument
+# normalize_name()
+
+def normalize_name_with_default(name='Default'):
+    """
+    Return name in lowercase
+    """
+    print(str(name).lower())
+normalize_name_with_default()
+print()
+
+# nothing returned from a function, re1 is None
+res1 = add_squares(4,21)
+print_hint('res1', res1)
+
+def add_squares_with_return(num1, num2):
+    """
+    Adds squares of two numbers
+    """
+    return num1**2 + num2**2
+
+# actualy returns a value that can be assigned to a variable
+res2 = add_squares_with_return(4,21)
+print_hint('res2', res2)
