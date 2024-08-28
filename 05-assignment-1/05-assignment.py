76ea5a882ea7656f5a28731da58ab9988f67d55d
Oleg Shevchenko <shevchenkoo.js@gmail.com>
2024-08-28 18:42:25 +0300

    feat: add assignment code

--- 05-assignment-1/05-assignment.py
+++ 05-assignment-1/05-assignment.py
@@ -28,5 +28,40 @@ def get_board(cell_number=3):
     board = [row] * cell_number
     return board  
   
-board1 = get_board(9)
+# board1 = get_board(9)
+# show_board(board1)
+
+def get_action_cell(board):
+    cells = []
+
+    for row in board:
+        cells.extend(row)
+    
+    available_cells = [ind + 1 for ind, el in enumerate(cells) if not bool(str(el).strip())]
+    chosen_cell_index = None
+
+    while type(chosen_cell_index) != int:
+        choice = input('Choose a cell to make a move: ')
+
+        if not choice.isdigit():
+            print('Please, provide a positive number')
+            continue
+        elif int(choice) not in range(1, len(cells) + 1):
+            print(f'Choose a number between 1 and {len(cells)}')
+            continue
+        elif int(choice) not in available_cells:
+            print(f'Cell {choice} was already picked, here\'s the board: ')
+            show_board(board)
+            continue
+        else:
+            chosen_cell_index = int(choice) - 1
+    
+    return chosen_cell_index
+
+row1 = [' ', 'x', ' ']
+row2 = ['o', 'x', ' ']
+row3 = [' ', 'x', 'o']
+board1 = [row1, row2, row3]
+
 show_board(board1)
+print(get_action_cell(board1))
