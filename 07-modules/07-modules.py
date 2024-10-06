357eca766dcf9c80e30a5dfa0a7a3153594094c7
Oleg Shevchenko <shevchenkoo.js@gmail.com>
2024-10-06 15:33:26 +0300

    feat: add practice code for __name__ property lesson

--- 07-modules/07-modules.py
+++ 07-modules/07-modules.py
@@ -12,3 +12,11 @@ print_number()
 # from Packages.Strings import module as module2
 from Packages.Strings.module import print_string
 print_string()
+
+# __name__ indicates how the module was run: directly or by being imported in other module and run from there
+print(f'__name__ in modules file being run directly is {__name__}')
+
+if __name__ == "__main__":
+    print('run directly')
+    # it is the common way to run scripts if module was run diirectly
+    # if module was imported you do not run anything except methods being used in the file that imports it
--- 07-modules/simple_module.py
+++ 07-modules/simple_module.py
@@ -1,2 +1,4 @@
 def simple_func():
     print('simple function from simple module')
+
+print(f'__name__ in simple_module file being imported is {__name__}')
